be6e4ba0c9b88c3052c0c4e9ca2f4c8d86ca976a
Saravanan <saravanagandis@gmail.com>
2021-08-26 00:27:45 +0530

    song added

--- src/javaprograms/Song.java
+++ src/javaprograms/Song.java
@@ -3,8 +3,9 @@ package javaprograms;
 public class Song {
 
 	public static void main(String[] args) {
-System.out.println("SPB");
-System.out.println("RajaSIR");
+		System.out.println("SPB");
+		System.out.println("RajaSIR");
+		System.out.println("Kamal Hassan");
 	}
 
 }
