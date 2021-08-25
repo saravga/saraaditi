1a645f67c20363b8a2bb1bc4eab32c2d7f4fd3c7
Saravanan <saravanagandis@gmail.com>
2021-08-25 23:36:52 +0530

    sara

--- src/javaprograms/Mergeconflict.java
+++ src/javaprograms/Mergeconflict.java
@@ -5,6 +5,8 @@ public class Mergeconflict {
 	public static void main(String[] args) {
 		
 		System.out.println("Sara Aditi");
+		System.out.println("Saranya");
+		System.out.println("Sara");
 
 	}
 
